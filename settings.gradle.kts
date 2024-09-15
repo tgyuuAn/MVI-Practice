bcce52f4bc4be92bc85ef91641d53d5cdd6e6f65
tgyuuAn <atg0614@gmail.com>
2024-09-15 23:01:37 +0900

    base : data, presentation, domain 모듈 분리

---
+++ settings.gradle.kts
@@ -0,0 +1,26 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "MVI practice"
+include(":app")
+include(":presentation")
+include(":data")
+include(":domain")
